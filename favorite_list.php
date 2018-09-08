9b8670808d56be0ce0bf63167f0b263850c3d80d
yakuo <prsal2253@gmail.com>
2018-09-08 13:55:30 +0800

    改php

---
+++ favorite_list.php
@@ -0,0 +1,121 @@
+<?php include 'page_item/head.php';?>
+</head>
+<body id="member" class="favorite_list">
+    <!-- top -->
+    <div class="index_top">
+    <?php include 'page_item/header.php';?> 
+</div>
+    <div class="index_main">
+        <!-- 麵包屑 -->
+        <!-- <section  class="bread_crumbs">
+            <ul>
+                <li><a href="#">home</a></li>
+                <li><a href="#">member</a></li>
+                <li>order</li>
+            </ul></section> -->
+    <section>
+    <div class="index_conten_flex">
+        <div class="index_conten_l">
+            <div class="item_01">
+                <div class="item_01menu item_01menu_in"><h6 class="transition">訂單列表</h6><span class="transition">Order List</span></div>
+                <div class="item_01menu "><a href="#"><h6 class="transition">會員資料</h6><span class="transition">member profile</span></a></div>
+                <div class="item_01menu"><a href="#"><h6 class="transition">追蹤清單</h6><span class="transition">my favourite</span></a></div>
+            </div>
+        </div>
+        <div class="index_conten_r">
+            <div class="member_title"><h2>My Favourit</h2><span>追蹤清單</span></div>
+            <div class="member_conten">
+                <div class="item_02 item_03 item_04">
+                    <!-- top -->
+                    <div class="item_02_conten">
+                        <p class="description">總共<span class="description_mark">32</span>筆商品</p>
+                    </div>
+
+                   
+                <!-- 商品 -->
+
+                <div class="item_02_conten">
+                        <div class="order_listbox">
+                                <div class="description_80">商品名稱</div>
+                                <div class="description_20">刪除</div>
+                        </div>
+
+                        <!-- 一件商品 -->
+                        <div class="order_listbox">
+                                <figure class="description_20"><a href="#"><img src="images/S-yellow-chair01-500px.png" alt="商品名稱"></a></figure>
+                                <div class="description_60">
+                                    <a href="#" class="product_name">Anastasia Tufted Chair - Christopher Knight HomeAnastasia Tufted Chair - Christopher Knight Home</a>
+                                    <p>黃色</p>
+                                    <p>$120000</p>
+                                </div>
+                                <div class="description_20"><div class="icon_garbage"></div></div>
+                        </div>
+
+                        <!-- 一件商品 -->
+                        <div class="order_listbox">
+                                <figure class="description_20"><a href="#"><img src="images/S-yellow-chair01-500px.png" alt="商品名稱"></a></figure>
+                                <div class="description_60">
+                                    <a href="#" class="product_name">Anastasia Tufted Chair - Christopher Knight HomeAnastasia Tufted Chair - Christopher Knight Home</a>
+                                    <p>黃色</p>
+                                    <p>$120000</p>
+                                </div>
+                                <div class="description_20"><div class="icon_garbage"></div></div>
+                        </div>
+
+                        <!-- 一件商品 -->
+                        <div class="order_listbox">
+                                <figure class="description_20"><a href="#"><img src="images/S-yellow-chair01-500px.png" alt="商品名稱"></a></figure>
+                                <div class="description_60">
+                                    <a href="#" class="product_name">Anastasia Tufted Chair - Christopher Knight HomeAnastasia Tufted Chair - Christopher Knight Home</a>
+                                    <p>黃色</p>
+                                    <p>$120000</p>
+                                </div>
+                                <div class="description_20"><div class="icon_garbage"></div></div>
+                        </div>
+
+                        <!-- 一件商品 -->
+                        <div class="order_listbox">
+                                <figure class="description_20"><a href="#"><img src="images/S-yellow-chair01-500px.png" alt="商品名稱"></a></figure>
+                                <div class="description_60">
+                                    <a href="#" class="product_name">Anastasia Tufted Chair - Christopher Knight HomeAnastasia Tufted Chair - Christopher Knight Home</a>
+                                    <p>黃色</p>
+                                    <p>$120000</p>
+                                </div>
+                                <div class="description_20"><div class="icon_garbage"></div></div>
+                        </div>
+
+                        <!-- 一件商品 -->
+                        <div class="order_listbox">
+                                <figure class="description_20"><a href="#"><img src="images/S-yellow-chair01-500px.png" alt="商品名稱"></a></figure>
+                                <div class="description_60">
+                                    <a href="#" class="product_name">Anastasia Tufted Chair - Christopher Knight HomeAnastasia Tufted Chair - Christopher Knight Home</a>
+                                    <p>黃色</p>
+                                    <p>$120000</p>
+                                </div>
+                                <div class="description_20"><div class="icon_garbage"></div></div>
+                        </div>
+
+            </div>
+
+            <!-- 頁碼 -->
+            <div class="page_num">
+                <ul>
+                    <li><a href="#"></a></li>
+                    <li>1</li>
+                    <li><a href="#">2</a></li>
+                    <li><a href="#">3</a></li>
+                    <li><a href="#"></a></li>
+                </ul>
+            </div>
+
+        </div>
+        </div>
+    </div>
+    </section>
+</div>
+<!-- footer -->
+<div class="index_footer">
+<?php include 'page_item/footer.php';?>
+</div>
+</body>
+</html>
--- login.php
+++ login.php
@@ -33,29 +33,20 @@ if(isset($_POST['email']) and isset($_POST['password'])) {
 }
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>login</title>
-    <link rel="stylesheet" href="css/reset.css">
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/member.css">
+<?php include 'page_item/head.php';?>
 </head>
 <body id="login" class="">
-    <div class="index_top">
-        <header><h1>palette</h1></header>
-    </div>
+<div class="index_top">
+    <?php include 'page_item/header.php';?> 
+</div>
     <div class="index_main">
         <!-- 麵包屑 -->
-        <section  class="bread_crumbs">
+        <!-- <section  class="bread_crumbs">
             <ul>
                 <li><a href="#">home</a></li>
                 <li><a href="#">member</a></li>
                 <li>login</li>
-            </ul></section>
+            </ul></section> -->
     <section  class="item_10">
         <div class="index_conten">
             <!--     登入成功與否訊息           -->
@@ -83,7 +74,7 @@ if(isset($_POST['email']) and isset($_POST['password'])) {
                     <!-- 登入 -->
                     <div class="index_conten_l">
                             <input type="text" name="email" placeholder="會員帳號">
-                            <input class="member_name" name="password" type="text" placeholder="密碼">
+                            <input class="member_name" name="password" type="password" placeholder="密碼">
                             <a href="#">忘記密碼</a>
                             <input type="submit" value="會員登入">
                             <input type="submit" class="fb_bnt" value="facebook登入">
@@ -128,6 +119,9 @@ if(isset($_POST['email']) and isset($_POST['password'])) {
             return isPass;
         }
     </script>
-<div class="index_footer"></div>
+<!-- footer -->
+<div class="index_footer">
+<?php include 'page_item/footer.php';?>
+</div>
 </body>
 </html>
