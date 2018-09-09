9b6dc423880b348224d1e53075b6bec324ede13b
orangejan <39645580+orangejan@users.noreply.github.com>
2018-09-09 14:17:04 +0800

    更新

--- signup_01.php
+++ signup_01.php
@@ -73,7 +73,7 @@
                         <p>消費者可來電洽詢本公司客服進行申請。</p></div>
                 </div>
             <div class="item_02_conten">
-                <a href="signup.php" class="palette_btn">我同意會員條款</a>
+                <a href="signup_02.php" class="palette_btn">我同意會員條款</a>
             </div>
             </div>
         </div>
